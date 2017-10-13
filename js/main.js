78ebc57aed031c96452e02973549b4e0add4d2c9
garima <garima8jun@gmail.com>
2017-10-13 06:33:12 +0530

    one page scroller

--- js/main.js
+++ js/main.js
@@ -5,31 +5,7 @@ $( document ).ready( function()
 		$( this ).attr( 'data-buzz' , $( this ).text() );
 	} );
     
-    
-    /*   if($('#oneblock').hasClass('opr-current')){ 
-           alert("one!");
-       }
-    
-      if($('#twoblock[data-target="1"]').hasClass('opr-current')){ 
-           alert("two!");
-       }
-    
-    
-      if($('#threeblock[data-target="2"]').hasClass('opr-current')){ 
-           alert("three!");
-       }
-    
-      if($('#fourblock[data-target="3"]').hasClass('opr-current')){ 
-           alert("four!");
-       }
-    
-      if($('#fiveblock[data-target="4"]').hasClass('opr-current')){ 
-           alert("five!");
-       }
-    
-      if($('#oneblock[data-target="5"]').hasClass('opr-current')){ 
-           alert("six!");
-       }*/
+
    
 } );
 
