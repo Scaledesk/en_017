227d9fd03ea63946f1a5d55c55473ed1e1577c2b
garima <garima8jun@gmail.com>
2017-10-16 17:05:27 +0530

    basic screen structure

--- js/main.js
+++ js/main.js
@@ -21,5 +21,61 @@ $( document ).ready( function()
 		});
 }).scroll();*/
 
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+        $('#collapseitem').toggleClass('openitem')
+	});
+});
+
+    
+/*    offcanvas menu*/
+
+    
+$('#hamburger-menu-icon-main').click(function(){
+//    $('#hamburger-menu-icon-main').toggleClass('active');
+    if($('i#hamburger-menu-icon-main').hasClass('active')){
+
+     /*  $('.navigation-mob').addclass('opacity');*/
+        
+        $('i#hamburger-menu-icon-main').removeClass('active');
+          document.getElementById("navigation-mob").style.display = "none";
+          $('.navigation-mob-child').animate({right:'-480px'});
+
+    }
+
+
+    else{
+       /*  $('.navigation-mob').addclass('opacity');*/
+        
+        $('i#hamburger-menu-icon-main').addClass('active');
+        
+          document.getElementById("navigation-mob").style.display = "block";
+        $('.navigation-mob-child').animate({right:'140px'});
+              
+       /* $('.navigation-mob').addClass('overlay');*/
+      /*   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";*/
+
+        return false;
+    }
+});
+
+$("#navigation-mob, .navigation-mob-child").click(function(){
+    
+    document.getElementById("navigation-mob").style.display = "none";
+    $('.navigation-mob-child').animate({right:'-480px'});
+    
+    $('i#hamburger-menu-icon-main').removeClass('active');
+    
+});
+
+
+
+
+
+
+
+
+
 
 
